951fad6b76fdf44d2e37f36a50ed5f8c6fe47ba8
nasriime <mnasr4040@gmail.com>
2018-11-18 06:48:24 +0200

    adding carousels and main section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,41 @@
-$("document").ready(function() {
+$(document).ready(function() {
+  $("#owl-1").owlCarousel({
+    rtl: true,
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    singleItem: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
 
-
-  
-
+  $("#owl-2").owlCarousel({
+    rtl: true,
+    margin: 15,
+    dots: false,
+    nav: true,
+    navText: [
+      "<img src='./images/right.png'>",
+      "<img src='./images/left.png'>"
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    }
+  });
 });
